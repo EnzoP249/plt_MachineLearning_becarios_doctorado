b7f2c85a5b4d7833b7e95f71756f82873c401fc1
EnzoPeña <enzo.lpge@gmail.com>
2026-01-16 16:16:44 -0500

    Se agregan líneas nuevas

--- plt_becarios_doctorado.py
+++ plt_becarios_doctorado.py
@@ -5,3 +5,20 @@ Created on Fri Jan 16 15:51:37 2026
 @author: Enzo
 """
 
+###############################################################################
+# PROYECTO PARA IMPLEMENTAR UN MODELO DE MACHINE LEARNING A UN BASE DE DATOS
+# SOBRE BECARIOS DE PROGRAMAS DE DOCTORADO
+###############################################################################
+
+###############################################################################
+# OBJETIVO DEL PROYECTO: DISEÑAR E IMPLEMENTAR UN ALGORITMO DE MACHINE LEARNING
+# PARA ESTABLECER UN MODELO PREDICTIVO
+###############################################################################
+
+# Se importan las librerías que serán usadas
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
